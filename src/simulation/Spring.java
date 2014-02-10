cc4bede08b1d24f64d9a6a99993c8efb9c5e709d
Kevin Do <kevin.kydat.do@gmail.com>
2014-02-09 23:02:06 -0500

    Remove unnecessary if/else block

--- src/simulation/Spring.java
+++ src/simulation/Spring.java
@@ -3,11 +3,11 @@ package simulation;
 import jgame.JGColor;
 
 public class Spring extends Muscle implements Force {
-	
-    private final static JGColor SPRING_COMPRESSED_COLOR = new JGColor(96, 96, 96);
-    private final static JGColor SPRING_STRETCHED_COLOR = new JGColor(208, 208, 208);
-    private final static double SPRING_WIDTH = 5.0;
-    protected float myRotation;
+
+	private final static JGColor SPRING_COMPRESSED_COLOR = new JGColor(96, 96, 96);
+	private final static JGColor SPRING_STRETCHED_COLOR = new JGColor(208, 208, 208);
+	private final static double SPRING_WIDTH = 5.0;
+	protected float myRotation;
 
 	public Spring(Mass a, Mass b, double restLength, double constant){
 		super("spring", a, b, restLength, constant, 0);
@@ -18,20 +18,12 @@ public class Spring extends Muscle implements Force {
 
 	@Override
 	public void paint(){
-		if(a != null){
-			if (a.distance(b) > restLength)
-				myEngine.drawLine(a.x, a.y, b.x, b.y, SPRING_WIDTH, SPRING_STRETCHED_COLOR);
-			else
-				myEngine.drawLine(a.x, a.y, b.x, b.y, SPRING_WIDTH, SPRING_COMPRESSED_COLOR);
-		}
-		else{
-			if (a.distance(b) > restLength)
-				myEngine.drawLine(a.x, a.y, b.x, b.y, SPRING_WIDTH, SPRING_STRETCHED_COLOR);
-			else
-				myEngine.drawLine(a.x, a.y, b.x, b.y, SPRING_WIDTH, SPRING_COMPRESSED_COLOR);
-		}
+		if (a.distance(b) > restLength)
+			myEngine.drawLine(a.x, a.y, b.x, b.y, SPRING_WIDTH, SPRING_STRETCHED_COLOR);
+		else
+			myEngine.drawLine(a.x, a.y, b.x, b.y, SPRING_WIDTH, SPRING_COMPRESSED_COLOR);
 	}
-	
+
 	@Override
 	public String getForceName(){
 		return "Spring";
