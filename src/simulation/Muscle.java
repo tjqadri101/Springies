aa8bbeb5df032e083c69d27dc2a6fd9ab3735606
Kevin Do <kevin.kydat.do@gmail.com>
2014-02-09 23:50:53 -0500

    Change Muscle to subclass of Spring

--- src/simulation/Muscle.java
+++ src/simulation/Muscle.java
@@ -1,40 +1,24 @@
 package simulation;
+
 import jgame.JGColor;
-import jgame.JGObject;
-import jgame.impl.JGEngineInterface;
 
-import org.jbox2d.common.Vec2;
+public class Muscle extends Spring implements Force{
 
-public class Muscle extends JGObject implements Force{
-
-	protected Mass a;
-	protected Mass b;
-	protected double restLength;
-	protected double constant;
+	protected double baseRestLength;
 	protected double amplitude;
 	protected double time;
 	protected double stretch;
 
-	protected JGEngineInterface myEngine;
 	private final static JGColor MUSCLE_POSITIVE_COLOR = JGColor.red;
 	private final static JGColor MUSCLE_NEGATIVE_COLOR = new JGColor(144, 0, 0);
 	private final static double MUSCLE_WIDTH = 5.0;
 	protected float myRotation;
 
 	public Muscle(Mass a, Mass b, double restLength, double constant, double amplitude){
-		this("muscle", a, b, restLength, constant, amplitude);
-	}
-	
-	public Muscle(String id, Mass a, Mass b, double restLength, double constant, double amplitude){
-		super(id, true, 300, 300, 8, null);
-
-		this.a = a;
-		this.b = b;
-		this.restLength = restLength;
-		this.constant = constant;
+		super("muscle", a, b, restLength, constant);
+		
+		this.baseRestLength = restLength;
 		this.amplitude = amplitude;
-
-		myEngine = eng;
 	}
 
 	@Override
@@ -44,23 +28,11 @@ public class Muscle extends JGObject implements Force{
 		else
 			myEngine.drawLine(a.x, a.y, b.x, b.y, MUSCLE_WIDTH, MUSCLE_NEGATIVE_COLOR);
 	}
-
+	
 	@Override
 	public void applyForce(Assembly assembly) {
-		for (Mass m : assembly.getMasses()){
-			updateStretch();
-			double curRestLength = restLength + stretch;
-
-			Vec2 displacement = new Vec2((float) (b.x - a.x), (float) (b.y - a.y));
-			displacement.normalize();
-			float magnitude = (float) (constant * Math.pow(a.distance(b) - curRestLength, 1));
-			Vec2 force = displacement.mul(magnitude);
-
-			if (m == a)
-				m.setForce(force.x, force.y);
-			else if (m == b)
-				m.setForce(-force.x, -force.y);
-		}
+		updateStretch();
+		super.applyForce(assembly);
 	}
 
 	public void changeAmplitude(double amount){
--- src/simulation/Spring.java
+++ src/simulation/Spring.java
@@ -1,8 +1,19 @@
 package simulation;
 
 import jgame.JGColor;
+import jgame.JGObject;
+import jgame.impl.JGEngineInterface;
 
-public class Spring extends Muscle implements Force {
+import org.jbox2d.common.Vec2;
+
+public class Spring extends JGObject implements Force {
+	
+	protected Mass a;
+	protected Mass b;
+	protected double restLength;
+	protected double constant;
+	
+	protected JGEngineInterface myEngine;
 
 	private final static JGColor SPRING_COMPRESSED_COLOR = new JGColor(96, 96, 96);
 	private final static JGColor SPRING_STRETCHED_COLOR = new JGColor(208, 208, 208);
@@ -10,9 +21,21 @@ public class Spring extends Muscle implements Force {
 	protected float myRotation;
 
 	public Spring(Mass a, Mass b, double restLength, double constant){
-		super("spring", a, b, restLength, constant, 0);
+		this("spring", a, b, restLength, constant);
 	}
 
+	public Spring(String id, Mass a, Mass b, double restLength, double constant){
+		super(id, true, 300, 300, 8, null);
+
+		this.a = a;
+		this.b = b;
+		this.restLength = restLength;
+		this.constant = constant;
+
+		myEngine = eng;
+	}
+	
+	
 	@Override
 	public void paint(){
 		if (a.distance(b) > restLength)
@@ -21,6 +44,22 @@ public class Spring extends Muscle implements Force {
 			myEngine.drawLine(a.x, a.y, b.x, b.y, SPRING_WIDTH, SPRING_COMPRESSED_COLOR);
 	}
 
+	@Override
+	public void applyForce(Assembly assembly) {
+		for (Mass m : assembly.getMasses()){
+
+			Vec2 displacement = new Vec2((float) (b.x - a.x), (float) (b.y - a.y));
+			displacement.normalize();
+			float magnitude = (float) (constant * Math.pow(a.distance(b) - restLength, 1));
+			Vec2 force = displacement.mul(magnitude);
+
+			if (m == a)
+				m.setForce(force.x, force.y);
+			else if (m == b)
+				m.setForce(-force.x, -force.y);
+		}
+	}
+	
 	@Override
 	public String getForceName(){
 		return "Spring";
